c8aba016f38cd05adc25be55a70e246532cc20f9
Leo Dube <leo.s.dube@gmail.com>
2022-11-19 11:50:14 -0800

    Add vercel.json

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,8 @@ from flask_cors import CORS
 from nhlapi import *
 
 app = Flask(__name__)
-CORS(app) # This will enable CORS for all routes
+CORS(app)  # This will enable CORS for all routes
+
 
 @app.route("/players/<player_id>")
 def player(player_id):
@@ -11,6 +12,7 @@ def player(player_id):
     See https://gitlab.com/dword4/nhlapi/-/blob/master/stats-api.md#people for more info'''
     res = get_player(player_id, params=request.args)
     return jsonify(res)
+
 
 @app.route("/players/<player_id>/stats")
 def playerstats(player_id):
@@ -22,34 +24,38 @@ def playerstats(player_id):
     res = get_player_stats(player_id, params=params)
     return jsonify(res)
 
+
 @app.route("/teams")
 def teams():
     '''Returns list of NHL teams
-    See https://gitlab.com/dword4/nhlapi/-/blob/master/stats-api.md#teams for more info''' 
+    See https://gitlab.com/dword4/nhlapi/-/blob/master/stats-api.md#teams for more info'''
     res = get_teams(params=request.args)
     return jsonify(res)
+
 
 @app.route("/teams/<team_id>")
 def team(team_id):
     '''Returns NHL team 
-    See https://gitlab.com/dword4/nhlapi/-/blob/master/stats-api.md#teams for more info''' 
+    See https://gitlab.com/dword4/nhlapi/-/blob/master/stats-api.md#teams for more info'''
     res = get_team(team_id, params=request.args)
     return jsonify(res)
+
 
 @app.route("/teams/<team_id>/stats")
 def teamstats(team_id):
     '''Returns NHL team stats
-    See https://gitlab.com/dword4/nhlapi/-/blob/master/stats-api.md#teams for more info''' 
+    See https://gitlab.com/dword4/nhlapi/-/blob/master/stats-api.md#teams for more info'''
     res = get_team_stats(team_id, params=request.args)
     return jsonify(res)
 
-# added
+
 @app.route("/team/<team_id>/roster")
 def teamroster(team_id):
     '''Return NHL team roster
     See https://gitlab.com/dword4/nhlapi/-/blob/master/stats-api.md#teams for more info'''
     res = get_team_roster(team_id, params=request.args)
     return jsonify(res)
+
 
 @app.route("/schedule")
 def schedule():
@@ -58,9 +64,10 @@ def schedule():
         - "startDate", "endDate" (must supply both)
         - "teamId"
         - "expand" e.g. "expand=schedule.linescore"
-    See https://gitlab.com/dword4/nhlapi/-/blob/master/stats-api.md#schedule for more info''' 
+    See https://gitlab.com/dword4/nhlapi/-/blob/master/stats-api.md#schedule for more info'''
     res = get_schedule(params=request.args)
     return jsonify(res)
+
 
 @app.route("/game/<game_pk>")
 def game(game_pk):
