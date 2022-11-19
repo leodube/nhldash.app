81d55a53b8244bc25e273ebc0e66dd8764880910
Leo Dube <leo.s.dube@gmail.com>
2022-11-19 12:31:04 -0800

    Rename backend folder to api
    And refactor a bit for vercel deploys

--- api/src/__init__.py
+++ api/src/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from nhlapi import *
+from .nhlapi import *
 
 app = Flask(__name__)
 CORS(app)  # This will enable CORS for all routes
